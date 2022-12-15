b491bc85bb5f3ac0146d3ea67cc5762a18a51655
mahdieh-br <120256314+mahdieh-br@users.noreply.github.com>
2022-12-15 13:31:09 +0100

    Corrected apiKey

--- src/app.js
+++ src/app.js
@@ -52,8 +52,8 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "49362a8e84916550cbb6ca0d82bf4818";
-  let apiUrl = `https://api.openweathermap.org/data/2.05/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiKey = "58a6775f97527351bf6c6966e209be39";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
@@ -85,7 +85,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "49362a8e84916550cbb6ca0d82bf4818";
+  let apiKey = "58a6775f97527351bf6c6966e209be39";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
